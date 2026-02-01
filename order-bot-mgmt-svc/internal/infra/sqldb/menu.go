d31de4e804661a28aa437128ea9f91cab920b46a
RockkLee <50697724+RockkLee@users.noreply.github.com>
2026-02-01 15:06:56 +0800

    Refactor menu stores to use executor

--- order-bot-mgmt-svc/internal/infra/sqldb/menu.go
+++ order-bot-mgmt-svc/internal/infra/sqldb/menu.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"order-bot-mgmt-svc/internal/infra/sqldb/pqsqldb"
+	"order-bot-mgmt-svc/internal/infra/sqldb/sqldbexecutor"
 	"order-bot-mgmt-svc/internal/models/entities"
 	"order-bot-mgmt-svc/internal/store"
 )
@@ -47,8 +48,12 @@ func NewMenuStore(db *pqsqldb.DB) *MenuStore {
 }
 
 func (s *MenuStore) FindByBotID(ctx context.Context, menuID string) (entities.Menu, error) {
+	execer, err := sqldbexecutor.Executor(s.db, nil)
+	if err != nil {
+		return entities.Menu{}, fmt.Errorf("sqldb.MenuStore.FindByBotID: %w", err)
+	}
 	var record MenuRecord
-	err := s.db.QueryRowContext(ctx, selectMenuByBotIDQuery, menuID).Scan(&record.ID, &record.BotID)
+	err = execer.QueryRowContext(ctx, selectMenuByBotIDQuery, menuID).Scan(&record.ID, &record.BotID)
 	if err != nil {
 		return entities.Menu{}, fmt.Errorf("sqldb.MenuStore.FindByBotID: %w", err)
 	}
@@ -56,12 +61,12 @@ func (s *MenuStore) FindByBotID(ctx context.Context, menuID string) (entities.Me
 }
 
 func (s *MenuStore) CreateMenu(ctx context.Context, tx store.Tx, menu entities.Menu) error {
-	sqlTx, ok := tx.(*sql.Tx)
-	if !ok {
-		return fmt.Errorf("sqldb.CreateMenu: %w", store.ErrInvalidTx)
+	execer, err := sqldbexecutor.Executor(s.db, tx)
+	if err != nil {
+		return fmt.Errorf("sqldb.MenuStore.CreateMenu: %w", err)
 	}
 	record := MenuRecordFromModel(menu)
-	_, err := sqlTx.ExecContext(ctx, insertMenuQuery, record.ID, record.BotID)
+	_, err = execer.ExecContext(ctx, insertMenuQuery, record.ID, record.BotID)
 	if err != nil {
 		return fmt.Errorf("sqldb.MenuStore.CreateMenu(), ExecContext: %w", err)
 	}
@@ -69,12 +74,12 @@ func (s *MenuStore) CreateMenu(ctx context.Context, tx store.Tx, menu entities.M
 }
 
 func (s *MenuStore) UpdateMenu(ctx context.Context, tx store.Tx, menu entities.Menu) error {
-	sqlTx, ok := tx.(*sql.Tx)
-	if !ok {
-		return fmt.Errorf("sqldb.UpdateMenu: %w", store.ErrInvalidTx)
+	execer, err := sqldbexecutor.Executor(s.db, tx)
+	if err != nil {
+		return fmt.Errorf("sqldb.MenuStore.UpdateMenu: %w", err)
 	}
 	record := MenuRecordFromModel(menu)
-	result, err := sqlTx.ExecContext(ctx, updateMenuQuery, record.ID, record.BotID)
+	result, err := execer.ExecContext(ctx, updateMenuQuery, record.ID, record.BotID)
 	if err != nil {
 		return fmt.Errorf("sqldb.MenuStore.UpdateMenu(), ExecContext: %w", err)
 	}
@@ -89,11 +94,11 @@ func (s *MenuStore) UpdateMenu(ctx context.Context, tx store.Tx, menu entities.M
 }
 
 func (s *MenuStore) DeleteMenu(ctx context.Context, tx store.Tx, menuID string) error {
-	sqlTx, ok := tx.(*sql.Tx)
-	if !ok {
-		return fmt.Errorf("sqldb.DeleteMenu(): %w", store.ErrInvalidTx)
+	execer, err := sqldbexecutor.Executor(s.db, tx)
+	if err != nil {
+		return fmt.Errorf("sqldb.MenuStore.DeleteMenu: %w", err)
 	}
-	result, err := sqlTx.ExecContext(ctx, deleteMenuQuery, menuID)
+	result, err := execer.ExecContext(ctx, deleteMenuQuery, menuID)
 	if err != nil {
 		return fmt.Errorf("sqldb.MenuStore.DeleteMenu(), ExecContext: %w", err)
 	}
