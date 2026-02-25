c3ba705814f276d5d59d992189429bac4d864fd4
SamLee <sam.lee@mobiigenius.com>
2026-02-25 15:13:03 +0800

    Set the ECR auto scan toggle to False

--- infra-terraform/modules/ecr/main.tf
+++ infra-terraform/modules/ecr/main.tf
@@ -4,10 +4,22 @@ resource "aws_ecr_repository" "this" {
   name                 = each.value
   image_tag_mutability = "MUTABLE"
 
+  # An image will be scanned the possible vulnerabilities when it's pushed to ECR.
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
 
+  # Push flow:
+  # 1) Client authenticates to ECR
+  # 2) Client pushes layers/manifest over HTTPS
+  # 3) ECR stores the image data and encrypts it
+  #
+  # Pull flow:
+  # 1) Client authenticates to ECR
+  # 2) ECR checks pull permission
+  # 3) ECR reads encrypted image data from storage
+  # 4) ECR decrypts it server-side automatically
+  # 5) ECR sends the image over HTTPS
   encryption_configuration {
     encryption_type = "AES256"
   }
@@ -17,20 +29,20 @@ resource "aws_ecr_repository" "this" {
   })
 }
 
-resource "aws_ecr_lifecycle_policy" "this" {
-  for_each = aws_ecr_repository.this
-
-  repository = each.value.name
-  policy     = jsonencode({
-    rules = [{
-      rulePriority = 1
-      description  = "Keep only the latest 20 images"
-      selection = {
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = 20
-      }
-      action = { type = "expire" }
-    }]
-  })
-}
+# resource "aws_ecr_lifecycle_policy" "this" {
+#   for_each = aws_ecr_repository.this
+#
+#   repository = each.value.name
+#   policy     = jsonencode({
+#     rules = [{
+#       rulePriority = 1
+#       description  = "Keep only the latest 20 images"
+#       selection = {
+#         tagStatus   = "any"
+#         countType   = "imageCountMoreThan"
+#         countNumber = 20
+#       }
+#       action = { type = "expire" }
+#     }]
+#   })
+# }
